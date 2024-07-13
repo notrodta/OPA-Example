0c91eab06c96d1423f513f80be19e6f887cdbe5c
Rod <rody101@gmail.com>
2024-07-13 17:58:37 -0400

    working version - express server uses opa to check api authz using axios

--- myapp/app.js
+++ myapp/app.js
@@ -38,44 +38,37 @@
 //   console.log(`Example app listening at http://localhost:${port}`);
 // });
 
-
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 3000;
 
 const opa_url = "http://localhost:8181";
 const policy_path = "/v1/data/httpapi/authz";
 
 const url = opa_url + policy_path;
 
+// check to see if bob can see alice salary
 app.get("/finance/salary/alice", async function (req, res) {
-    const options = {
-        method: "POST",
-        header: {
-            Accept: "appliction/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            method: "GET",
-            path: ["finance", "salary", "alice"],
-            user: "bob"
-        })
-    };
+  try {
+    const response  = await axios.post(url, {
+      input: {
+        method: "GET",
+        path: ["finance", "salary", "alice"],
+        user: "bob",
+      },
+    });
+    res.status(200).json({ data: response.data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Internal Server Error..." });
+  }
+});
 
-    try {
-        let response = await fetch(url, options);
-        response = await response.json();
-        res.status(200).json(response);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: 'Internal Server Error...' });
-    }
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
