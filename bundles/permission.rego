fa64ecbbfea8fa3cd28da725e9ecd201d2efb4a4
Rod <rody101@gmail.com>
2024-07-16 12:22:59 -0400

    implemented opa express middleware from build security

--- bundles/permission.rego
+++ bundles/permission.rego
@@ -7,9 +7,23 @@ import data.role_permissions # // import role_permissions from data.json
 default allow = false
 
 allow if {
+    print(input)
     print("permission");
-    role := input.subject.roles[_] # // for every input roles
-    permissions := role_permissions[role] # // get permission of the role
-    permission := permissions[_] # // for every permission
-    permission == {"action": input.action, "object": input.object} # // allow = true when input action and object matches
+    print(role_permissions)
+    print(input.request.path)
+    print(input.resources.attributes)
+    print(role_permissions)
+    permission := role_permissions[_]
+    print("permission start")
+    print(input.resources.permissions)
+    print(permission)
+    print("permission start")
+    permission == input.resources.permissions[0]
+    # role := input.subject.roles[_] # // for every input roles
+    # permissions := role_permissions[role] # // get permission of the role
+    # permission := permissions[_] # // for every permission
+    # print("list permission start")
+    # print(permission)
+    # print("list permission start")
+    # permission == {"action": input.action, "object": input.object} # // allow = true when input action and object matches
 }
--- myapp/app.js
+++ myapp/app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const axios = require("axios");
-const createOpaMiddleware = require("./opa");
+const bodyParser = require('body-parser');
+const extAuthz = require('@build-security/opa-express-middleware');
 
 const app = express();
 const port = 3000;
 
-// const opa_url = "http://localhost:8181";
 const opa_url = "http://opa:8181";
-// const opa_url = "http://127.0.0.1:8181";
 const policy_path = "/v1/data/httpapi/authz";
 
 const url = opa_url + policy_path;
@@ -15,34 +14,20 @@ const url = opa_url + policy_path;
 const access_token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJyb2xlcyI6WyJ1c2VyIl19.LW5pvc1TRaIk3FNaMADQmjx2Hy86weS64WtVdgK1oZk";
 
-const hasPermission = createOpaMiddleware("http://opa:8181");
+  const extAuthzMiddleware = extAuthz.authorize((req) => ({
+    port: 8181,
+    hostname: 'http://opa',
+    policyPath: 'httpapi/authz/allow',
+    // enable: req.method === "GET",
+    // enrich: { serviceId: 1 }
+}));
 
-// check if the request is allowed to take action "read" to object "order"
-app.get("/orders/:id", hasPermission("read", "order"),  (req, res) => {
-  res.json({ message: `you can read order with id ${req.params.id}` });
+app.use(bodyParser.json());
+
+app.get('/region/:region/users/:userId', extAuthz.permissions('read'), extAuthzMiddleware, (req, res) => {
+    res.send('allowed');
 });
 
-// check if the request is allowed to take action "create" to object "order"
-app.post("/orders", hasPermission("create", "order"), (req, res) => {
-  res.json({ message: `you can create order` });
-});
-
-// check to see if bob can see alice salary
-app.get("/finance/salary/alice", async function (req, res) {
-  try {
-    const response = await axios.post(url, {
-      input: {
-        method: "GET",
-        path: ["finance", "salary", "alice"],
-        user: "bob",
-      },
-    });
-    res.status(200).json({ data: response.data });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: "Internal Server Error..." });
-  }
-});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
